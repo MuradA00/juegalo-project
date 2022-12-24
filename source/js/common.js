ccd737bffcb5352e6a35a248ef4be25d2d3270bd
Murad <abishlimurad@gmail.com>
2022-12-25 01:15:25 +0200

    almost last changes

--- source/js/common.js
+++ source/js/common.js
@@ -1,11 +1,28 @@
 const main = document.querySelector('main'),
       header = document.querySelector('.header'),
       controlPanel = document.querySelector('.control'),
-      toggles = document.querySelectorAll('.control__toggle');
+      toggles = document.querySelectorAll('.control__toggle'),
+      graphDisplay = document.querySelector('.graph'),
+      burger = document.querySelector('.header__burger'),
+      menu = document.querySelector('.menu'),
+      closeMenuIcon = document.querySelector('.menu__close'),
+      body = document.body;
 
 function setSpacing() {
   const headersHeight = header.clientHeight;
   main.style.marginTop = `${headersHeight}px`;
+}
+
+function closeMenu() {
+  menu.classList.remove('show-menu');
+  body.style.overflow = '';
+  burger.classList.remove('active-burger')
+}
+
+function showMenu() {
+  burger.classList.toggle('active-burger');
+  body.style.overflow = 'hidden';
+  burger.classList.contains('active-burger') ? menu.classList.add('show-menu') : menu.classList.remove('show-menu')
 }
 
 function changeToggle() {
@@ -14,7 +31,13 @@ function changeToggle() {
       toggles.forEach(elem => elem.classList.remove('selected-toggle'))
       this.classList.add('selected-toggle')
       const autoToggle = document.querySelector('.auto-toggle');
-      autoToggle.classList.contains('selected-toggle') ? controlPanel.classList.add('manual-toggle-selected') : controlPanel.classList.remove('manual-toggle-selected');
+      if (autoToggle.classList.contains('selected-toggle')) {
+        controlPanel.classList.add('manual-toggle-selected')
+        graphDisplay.classList.add('graph-active')
+      } else {
+        controlPanel.classList.remove('manual-toggle-selected')
+        graphDisplay.classList.remove('graph-active')
+      }
     })
   })
 }
@@ -22,3 +45,5 @@ function changeToggle() {
 changeToggle();
 
 document.addEventListener('DOMContentLoaded', setSpacing);
+burger.addEventListener('click', showMenu);
+closeMenuIcon.addEventListener('click', closeMenu);
