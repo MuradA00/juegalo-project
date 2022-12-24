ad9436b6e382d84f1c20f8c0ccc2bed02012e30c
Murad <abishlimurad@gmail.com>
2022-12-24 09:02:38 +0200

    another changes

--- source/js/common.js
+++ source/js/common.js
@@ -1,9 +1,24 @@
 const main = document.querySelector('main'),
-      header = document.querySelector('.header');
+      header = document.querySelector('.header'),
+      controlPanel = document.querySelector('.control'),
+      toggles = document.querySelectorAll('.control__toggle');
 
 function setSpacing() {
   const headersHeight = header.clientHeight;
   main.style.marginTop = `${headersHeight}px`;
 }
+
+function changeToggle() {
+  toggles.forEach(toggle => {
+    toggle.addEventListener('click', function() {
+      toggles.forEach(elem => elem.classList.remove('selected-toggle'))
+      this.classList.add('selected-toggle')
+      const autoToggle = document.querySelector('.auto-toggle');
+      autoToggle.classList.contains('selected-toggle') ? controlPanel.classList.add('manual-toggle-selected') : controlPanel.classList.remove('manual-toggle-selected');
+    })
+  })
+}
+
+changeToggle();
 
 document.addEventListener('DOMContentLoaded', setSpacing);
